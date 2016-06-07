866f890427f546f10a6a0dec984d127c507f162f
Ryan Ackett <Ryan@Emily-Allens-MacBook-Pro-2.local>
2016-06-07 15:39:03 -0400

    Saving

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+from django.contrib import admin 
+# import your model 
+from collection.models import Category 
 
-# Register your models here.
+# set up automated slug creation
+class CategoryAdmin(admin.ModelAdmin): 
+    model = Category 
+    list_display = ('name', 'description',) 
+    prepopulated_fields = {'slug': ('name',)}
+
+# and register it 
+admin.site.register(Category, CategoryAdmin)
