86b06cd39f6d15080071ff0368f5c4a6cfd60050
Ryan Ackett <Ryan@Emily-Allens-MacBook-Pro-2.local>
2016-06-08 11:26:50 -0400

    Created DB: parts3.db

--- collection/views.py
+++ collection/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from collection.models import Category
 
 # Create your views here.
 def index(request):
     number = 6
+    categories = Category.objects.all()
+    
     return render( request, 'index.html', { 
         'number': number,
+        'categories': categories,
     })
